3fadd51473249c17bb9d0039bd6f933593ba8c0c
AngeloDima <Angelodimauro91@gmail.com>
2023-02-03 12:36:10 +0100

    Milestone 2   Evento istanziato e ho chiesto all'utente i vari parametri SENZA CONTROLLO

--- src/Gestore_eventi/Main.java
+++ src/Gestore_eventi/Main.java
@@ -8,16 +8,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Evento nuovoEvento = new Evento("evento", 03/02/2023, 100, 0);
+		
+		Scanner s = new Scanner (System.in);
+		String nomeEvento;
+		String PostiDaPrenotare;
+		LocalDate today = LocalDate.now();
+		
+		
+		System.out.println("nome evento: ");
+		nomeEvento = s.nextLine();
+		
+		System.out.println("inserisci data: ");
+		String dataInput = s.nextLine();
+        LocalDate data = LocalDate.parse(dataInput, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-		
-		
-		
-		
-		
-		
-		
+        System.out.println("Quanti posti vuoi prenotare?: ");
+        PostiDaPrenotare = s.nextLine();
         
-
+        System.out.println(nomeEvento + dataInput + PostiDaPrenotare);
+        
+        
+        
+        
 	}
 
 }
